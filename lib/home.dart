87696911ac38f3559f83ccd5ce19609af6661ed2
Jie Li <jie.li@jliconsulting.no>
2020-04-10 18:39:30 +0200

    finish 102

--- lib/home.dart
+++ lib/home.dart
@@ -13,19 +13,99 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/product.dart';
+import 'model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
-  // TODO: Make a collection of cards (102)
   // TODO: Add a variable for Category (104)
+
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: ADjust card heights (103)
+        child: Column(
+          // TODO: Center (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Expanded(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      // TODO: Align labels to the bottom and center (103)
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // TODO: Change innermost Column (103)
+                      children: <Widget>[
+                        // TODO: Handle overflowing labels (103)
+                        Text(
+                          product.name,
+                          style: theme.textTheme.headline6,
+                          maxLines: 1,
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          formatter.format(product.price),
+                          style: theme.textTheme.bodyText2,
+                        )
+                      ],
+                    ))),
+          ],
+        ),
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu, semanticLabel: 'menu'),
+          onPressed: () {
+            print('Menu Button');
+          },
+        ),
+        title: Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search, semanticLabel: 'search'),
+              onPressed: () {
+                print('Search Button');
+              }),
+          IconButton(
+              icon: Icon(Icons.tune, semanticLabel: 'filter'),
+              onPressed: () {
+                print('Filter Button');
+              }),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(context),
       ),
       resizeToAvoidBottomInset: false,
     );
